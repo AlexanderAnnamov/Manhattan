486c74789316b31353ef5460a8f37c62e1bbe942
HyliBleat <pozdarovaites@mail.ru>
2023-02-22 20:17:40 +0500

    end

--- js/main.js
+++ js/main.js
@@ -23,30 +23,30 @@ const btnAds2 = document.querySelector('#btn-ads-2');
 const btnNext = document.querySelector('#btn_next');
 const btnPrev = document.querySelector('#btn-prev');
 
-btnPrev.addEventListener('mouseover', function (){
-    document.getElementById("btn-prev-img").src="./img/top-menu/orange-prev-btn.svg";
+btnPrev.addEventListener('mouseover',  () => {
+    document.getElementById("btn-prev-img").src="./img/Index/top-menu/orange-prev-btn.svg";
 });
 
 btnPrev.addEventListener('mouseout', function (){
-    document.getElementById("btn-prev-img").src="./img/top-menu/prev-btn.svg";
+    document.getElementById("btn-prev-img").src="./img/Index/top-menu/prev-btn.svg";
 });
 
 btnNext.addEventListener('mouseover', function (){
-    document.getElementById("btn-next-img").src="./img/top-menu/orange-next-btn.svg";
+    document.getElementById("btn-next-img").src="./img/Index/top-menu/orange-next-btn.svg";
 });
 
 btnNext.addEventListener('mouseout', function (){
-    document.getElementById("btn-next-img").src="./img/top-menu/next-btn.svg";
+    document.getElementById("btn-next-img").src="./img/Index/top-menu/next-btn.svg";
 });
 
-
+// toggle
 btnAds1.addEventListener('click', function (){
     btnAds1.classList.remove('announcement__ads-txt_change');
     btnAds2.classList.add('announcement__ads-txt_change');
-    document.getElementById("img-ads-1").src="./img/announcement/slider_image_1.jpg";
-    document.getElementById("img-ads-2").src="./img/announcement/slider_image_2.jpg";
-    document.getElementById("img-ads-3").src="./img/announcement/slider_image_3.png";
-    document.getElementById("img-ads-4").src="./img/announcement/slider_image_4.png";
+    document.getElementById("img-ads-1").src="./img/Index/announcement/slider_image_1.jpg";
+    document.getElementById("img-ads-2").src="./img/Index/announcement/slider_image_2.jpg";
+    document.getElementById("img-ads-3").src="./img/Index/announcement/slider_image_3.png";
+    document.getElementById("img-ads-4").src="./img/Index/announcement/slider_image_4.png";
 
 });
 
@@ -54,10 +54,13 @@ btnAds1.addEventListener('click', function (){
 btnAds2.addEventListener('click', function (){
     btnAds2.classList.remove('announcement__ads-txt_change');
     btnAds1.classList.add('announcement__ads-txt_change');
-    document.getElementById("img-ads-1").src="./img/announcement/slider_image_5.jpg";
-    document.getElementById("img-ads-2").src="./img/announcement/slider_image_6.jpg";
-    document.getElementById("img-ads-3").src="./img/announcement/slider_image_7.jpg";
-    document.getElementById("img-ads-4").src="./img/announcement/slider_image_8.jpg";
+    document.getElementById("img-ads-1").src="./img/Index/announcement/slider_image_5.jpg";
+    document.getElementById("img-ads-2").src="./img/Index/announcement/slider_image_6.jpg";
+    document.getElementById("img-ads-3").src="./img/Index/announcement/slider_image_7.jpg";
+    document.getElementById("img-ads-4").src="./img/Index/announcement/slider_image_8.jpg";
 });
 
-
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+});
